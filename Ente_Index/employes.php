d60b2b9e656407fafa1b054084defee5a0ead0cc
ARAYNAUD <adrien.raynaud@ynov.com>
2023-01-26 14:10:44 +0100

    employés html

---
+++ Ente_Index/employes.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="style2.css">
+
+</head>
+<body>
+    <div class="container">
+        <?php
+        include '../config.php';
+        $sql = "SELECT * FROM employes";
+        $result = $conn->query
+        ($sql);
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                $nom = $row["nom"];
+                echo "
+                    <div class='card'>
+                        <div class='face face1'>
+                            <div class='content'>
+                                <img src='https://static.vecteezy.com/ti/vecteur-libre/p1/4274237-person-icon-user-interface-icon-silhouette-of-man-simple-symbol-outline-symbol-in-your-web-site-design-logo-app-ui-webinar-video-chat-vectoriel.jpg'>
+                                <h3> $nom </h3>
+                            </div>
+                        </div>
+                        <div class='face face2'>
+                            <div class='content'>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+                            </div>
+                        </div>
+                    </div>";
+            }
+        } else {
+            echo "0 results";
+        }
+        $conn->close();
+        ?>
+    </div>
+</body>image.png
+</html>
+
+
--- Ente_Index/index.php
+++ Ente_Index/index.php
@@ -1,38 +1,41 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style1.css">
     <title>Entreprises</title>
 </head>
+
 <body>
     <section class="hero-section">
-    <div class="card-grid">
-        <?php
-        include '../config.php';
-        $sql = "SELECT * FROM etablissement";
-        $result = $conn->query($sql);
-        if ($result->num_rows > 0) {
-            // output data of each row
-            while($row = $result->fetch_assoc()) {
-                ?>
-                    <a class="card" href="#">
-                    <div class="card__background" style="background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQB-cgI1KYZQ3r3Gwb1tTwhq37A_JsG2w2IkA&usqp=CAU)"></div>
-                    <div class="card__content">
-                        <p class="card__category">Entreprises</p>
-                        <h3 class="card__heading"></h3>
-                    </div>
-                </a>
-                <?php
+        <div class="card-grid">
+            <?php
+            include '../config.php';
+            $sql = "SELECT * FROM etablissement";
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0) {
+                // output data of each row
+                while ($row = $result->fetch_assoc()) {
+                    $nom = $row["nom"];
+                    echo "<a class='card' href='#'>
+                        <div class='card__background'
+                            style='background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQB-cgI1KYZQ3r3Gwb1tTwhq37A_JsG2w2IkA&usqp=CAU)'>
+                        </div>
+                        <div class='card__content'>
+                            <p class='card__category'>Entreprises</p>
+                            <h3 class='card__heading'> $nom </h3>
+                        </div>
+                    </a>";
+                }
+            } else {
+                echo "0 results";
             }
-        } else {
-            echo "0 results";
-        }
-        $conn->close();
-        ?>
-    </div>
-            </section>
+            $conn->close();
+            ?>
+        </div>
+    </section>
     </div>
 </body>
+
 </html>
