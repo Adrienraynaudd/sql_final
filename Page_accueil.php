821c237dd83401ddee00bef8c6c64dbba2d838d3
Nathan BOURRY <nathan.bourry@ynov.com>
2023-01-29 15:14:57 +0100

    page d'acceuil

---
+++ Page_accueil.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css" />
+        <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
+        <meta name="viewport" content="width=device-width">
+    </head>
+    <body>
+        <div class="Bandeau">
+        <img class="logo" src="logo.png" alt="Logo">
+        </div>
+        <div class="typewriter-container">
+            <div class="typewriter">
+                <p>BIENVENUE sur votre CRM.</p>
+            </div>
+        </div>
+        <div class="Cubes_links">
+            <div class="column" style="background-color: #32607A "><p>💼<br>&emsp;<br> Employés</p></div>
+            <div class="column" style="background-color:#32607A"><p>👔<br>&emsp;<br> Postes</p></div>
+            <div class="column" style="background-color:#32607A"><p>🏭<br>&emsp;<br> Etablissement</p></div>
+        </div>
+    </body>
+</html>
