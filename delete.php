cd9579bec5857eaaeb0487d18b49597f163a2b60
ARAYNAUD <adrien.raynaud@ynov.com>
2023-01-31 14:32:45 +0100

    final

--- delete.php
+++ delete.php
@@ -4,4 +4,4 @@ $id = $_GET['id'];
 $sql = "DELETE FROM employes WHERE id_employe = $id";
 $result = $conn->query($sql);
 
-header('Location: ../front/entreprise.php');
+header('Location: ../front/employes.php');
